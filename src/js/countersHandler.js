f3510d74c882fb4d9ddd022d723aab86e231286d
shurikation <shurikation@gmail.com>
2020-06-24 17:30:06 +0400

    Added facts area section

--- src/js/countersHandler.js
+++ src/js/countersHandler.js
@@ -1,43 +1,44 @@
 const $factsArea = document.querySelector('.gallery');
 
-const countersDataBase = [
+const countersSettings = [
   {
     $element: document.querySelector('.fact-num--1'),
     totalValue: 3560,
-    intervalDelay: 30,
+    intervalDelay: 15,
     stepValue: 73,
   },
   {
     $element: document.querySelector('.fact-num--2'),
     totalValue: 195,
-    intervalDelay: 60,
+    intervalDelay: 30,
     stepValue: 4,
   },
   {
     $element: document.querySelector('.fact-num--3'),
     totalValue: 455,
-    intervalDelay: 60,
+    intervalDelay: 30,
     stepValue: 9,
   },
   {
     $element: document.querySelector('.fact-num--4'),
     totalValue: 15,
-    intervalDelay: 200,
+    intervalDelay: 110,
     stepValue: 1,
   },
 ];
 
 let countersWasScrolled = true;
+
 function isCountersShouldBeRunning() {
   if (isBlockScrolledDown($factsArea) && countersWasScrolled) {
     countersWasScrolled = false;
-    countersDataBase.forEach(data => runFastCounter(data));
+    countersSettings.forEach(data => runFastCounter(data));
   } else {
     return false;
   }
 }
 
-//Первая часть счетчика - быстрая
+//The first phase of the Counter - Fast phase
 function runFastCounter({$element, intervalDelay, totalValue, stepValue}) {
   let counter = 0;
   const integerStepValue = parseInt(stepValue);
@@ -52,7 +53,7 @@ function runFastCounter({$element, intervalDelay, totalValue, stepValue}) {
   }, intervalDelay);
 }
 
-//Вторая часть счетчика - медленная
+//The last phase of the counter - Slow phase
 function runSlowCounter($element, totalValue, intervalDelay, stepValue = 1) {
   let counter = parseInt($element.innerText);
   const ID2 = setInterval(() => {
