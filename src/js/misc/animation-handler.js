2475a9fdcae38730bd4a9c60ed62777d1b4b741f
shurikation <shurikation@gmail.com>
2020-07-21 21:14:12 +0400

    Expanded Animation Handler functionality

--- src/js/misc/animation-handler.js
+++ src/js/misc/animation-handler.js
@@ -1,6 +1,40 @@
-export class AnimationDurationHandler {
-  static getDuration(selector, varName) {
-    const value = getComputedStyle(document.querySelector(selector)).getPropertyValue(varName);
+export class AnimationHandler {
+  constructor() {
+    this.$promoImg = document.querySelector('.promo__img');
+    this.$promoTitle = document.querySelector('.promo__title');
+    this.$promoSubtitle = document.querySelector('.promo__subtitle');
+    this.$promoButton = document.querySelector('.promo__button-buy');
+    this.init();
+  }
+
+  init() {
+    this.preloaderHandler();
+  }
+
+  preloaderHandler() {
+    window.onload = () => {
+      document.body.classList.add('loaded_hiding');
+      this.promoAnimationsHandler();
+      window.setTimeout(() => {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+      }, 500);
+    };
+  }
+
+  promoAnimationsHandler() {
+    this.$promoImg.classList.add('fadeInRight');
+    this.$promoTitle.classList.add('fadeInUp');
+    this.$promoSubtitle.classList.add('fadeInUp');
+    this.$promoButton.classList.add('fadeInLeft')
+  }
+
+  static getDuration(selector, cssVar) {
+    const value = getComputedStyle(document.querySelector(selector)).getPropertyValue(cssVar);
     return +value.match(/\d/g).join('');
   }
+
+
+
+
 }
